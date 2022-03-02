c700942bb84b787e20c64bf448396841a562eb56
Uday <unayak900@gmail.com>
2022-03-02 15:30:33 +0530

    NA got replaced with 01 -01 -1900

--- src/main/scala/SparkMovies.scala
+++ src/main/scala/SparkMovies.scala
@@ -1,22 +1,65 @@
+
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, regexp_replace, udf}
 
-import java.text.SimpleDateFormat
 import java.util.Locale
-
+//import java.sql.Date
+import java.text.SimpleDateFormat
 
 
 object SparkMovies {
 
   def getDateFromString(date:String): java.sql.Date =
-    {
+  {
+
     val formatter = new SimpleDateFormat("MMMMM dd yyyy", Locale.ENGLISH)
     val newformatted = new java.sql.Date(formatter.parse(date).getTime)
     return newformatted
-     }
+  }
+
+ /* val formatter = new SimpleDateFormat("MMMMM dd yyyy", Locale.ENGLISH)
+  val dateregex = "^\\d{5}-\\d{2}-\\d{2}$"
+  val dateonly = dateregex.r
+
+  def getDateFromString(stringdate: String):  java.sql.Date = {
+    val newformatted = new java.sql.Date(formatter.parse(stringdate).getTime)
+
+     stringdate.trim match
+    {
+      case dateonly() => newformatted
+      case e:DateFormatException => errorHandler(e)
+
+    }
+    getDateFromString(stringdate)
+  }*/
 
 
-    val getDateFromString_udf = udf(getDateFromString(_))
+  /*val formatter = new SimpleDateFormat("MMMMM dd yyyy")
+  def getDateFromString(stringdate: String): Either[String, java.util.Date] =
+ {
+   Try {
+     formatter.parse(stringdate).getTime
+   }
+      match {
+     case Success(s) => Right(s)
+     case Failure(e: ParseException) => Left(s"bad format:$stringdate")
+     case Failure(e: Throwable) => Left(s"Unknown error formatting: $stringdate")
+   }
+   getDateFromString(stringdate)
+ }*/
+
+  val getDateFromString_udf = udf(getDateFromString(_))
+
+  /*case ioException:IOException => Left(ioException)
+case e: Exception => Left(e)
+val formatter = new SimpleDateFormat("MMMMM dd yyyy", Locale.ENGLISH)
+val newformatted = new java.sql.Date(formatter.parse(date).getTime)
+return newformatted*/
+
+
+
+
 
 
   def main(args: Array[String]): Unit = {
@@ -40,12 +83,16 @@ object SparkMovies {
 
     val df2 = newdf.withColumn("ReleaseDate",regexp_replace(col("ReleaseDate"),",",""))
     df2.show(2,false)
+
+    val df3 = df2.withColumn("ReleaseDate",regexp_replace(col("ReleaseDate"),"NA","January 01 1900"))
     // Date function
 
 
-    val df3 = df2.limit(2)
-    val df4 = df3.withColumn("ReleaseDate",getDateFromString_udf(col("ReleaseDate")))
-    df4.show(2,false)
+    val df4 = df3.limit(5)
+    val df5 = df4.withColumn("ReleaseDate", getDateFromString_udf(col("ReleaseDate")))
+    df5.show(5,false)
+
+
     /*var strDate = "December 16 2015"
     //var format = DateTimeFormat.forPattern("MMM DD yyyy")
     val formatter = new SimpleDateFormat("MMM DD yyyy",Locale.ENGLISH)
@@ -53,6 +100,8 @@ object SparkMovies {
    // var date = format.parseDateTime(strDate)
     var td = trimLeft(newformatted)
     println("Here is the date with newformatted", td)*/
+
+
 
 
   }
